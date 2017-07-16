46396e3f6077ab308ab3b9ecd41e3bcf84c64b70
Kalaiarasan <skl.rsn@gmail.com>
2017-07-16 10:03:38 +0300

    Added Toml chages

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
+	"github.com/naoina/toml"
 	"sklrsn.github.com/Robot/store"
 )
 
@@ -13,10 +15,31 @@ var (
 	DATABASE = "postgres"
 )
 
+type Config struct {
+	Title    string `toml:"title"`
+	Database struct {
+		Username string `toml:"username"`
+		Password string `toml:"password"`
+		Database string `toml:"database"`
+	} `toml:"database"`
+}
+
 func main() {
+	file, err := os.Open("server-config.toml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var config Config
+	if err := toml.NewDecoder(file).Decode(&config); err != nil {
+		panic(err)
+	}
+
 	connection := store.Connection{}
-	connection.Initialize(USERNAME, PASSWORD, DATABASE)
+	connection.Initialize(string(config.Database.Username), string(config.Database.Password), string(config.Database.Database))
 
 	product := store.Product{Name: "Bat", Price: 27}
 	fmt.Println(store.Persist(&product, &connection))
+
 }
